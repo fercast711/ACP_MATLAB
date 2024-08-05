ed5449c3e6d077a0a82f32766ed52283ae24b54f
fercast711 <fercast7.fc@gmail.com>
2024-08-05 12:48:42 -0600

    step 1 and step 2 completed

--- ACP.m
+++ ACP.m
@@ -4,9 +4,14 @@ opts = detectImportOptions('EjemploEstudiantes.csv');
 opts = setvartype(opts, 'double');
 opts = setvaropts(opts, 'DecimalSeparator', ',');
 df = readtable("EjemploEstudiantes.csv", opts);
-disp(df);
 %% Paso 1: Centrar y reducir la tabla original de datos
+matrix_df = df{:,2:end};
+mean_matrix = mean(matrix_df);
+std_matrix = std(matrix_df);
+matrix_standardized = (matrix_df - mean_matrix)./std_matrix;
 %% Paso 2: Realizar el calculo de la matriz de correlaciones
+[n, m] = size(matrix_standardized);
+matrix_cov = ((matrix_standardized')*(matrix_standardized)) / (n-1);
 %% Paso 3: Calcular y ordenar (de mayor a menor) los vectores y valores propios de la matriz
 %% Paso 4: Considerando los valores y vectores propios construir una matriz
 %% Paso 5: Calcular la matriz de componentes principales
